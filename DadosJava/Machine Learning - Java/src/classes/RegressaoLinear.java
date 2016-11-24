857a97e8f54aa7b09631d73d9f9b7c3da06cf321
Aulas <Aulas>
2016-11-23 23:29:43 -0200

    Primeiro Commit

---
+++ DadosJava/Machine Learning - Java/src/classes/RegressaoLinear.java
@@ -0,0 +1,18 @@
+package classes;
+
+/*
+    y = a * bx
+    
+    Gradient Descent = 1/(2m) * E(ho(x) - y)^ 2
+    ho(x) = a * bx
+
+*/
+
+public class RegressaoLinear {
+    
+    private double t0;
+    private double t1;
+    
+    
+    
+}
